0ddcf0b6c3e55063d4373e4b4daaf6647519faa8
k24koba <hozu.kazu.hozu@gmail.com>
2019-04-18 01:51:16 +0900

    update motor_control node

--- scripts/motor_control.py
+++ scripts/motor_control.py
@@ -18,13 +18,34 @@ from whipbot.msg import Posture_angle
 from kondo_b3kondo_b3mservo_rosdriver.msg import Servo_command
 from kondo_b3kondo_b3mservo_rosdriver.msg import Servo_info
 
+battery_voltage_warn_flag = 0
+battery_voltage_fatal_flag = 0
+BATTERY_VOLTAGE_WARN = 14.2
+BATTERY_VOLTAGE_FATAL = 13.8
+
 
 def callback_get_servo_info(info):
+    global battery_voltage_warn_flag, battery_voltage_fatal_flag
     encoder_count = info.encoder_count
-    voltage = info.voltage
+    battery_voltage = info.motor_voltage
+    velocity = info.motor_velocity
+
+    if battery_voltage < BATTERY_VOLTAGE_WARN and battery_voltage_warn_flag == 0:
+        rospy.logwarn('battery voltage is low !')
+        battery_voltage_warn_flag = 1
+    elif battery_voltage < BATTERY_VOLTAGE_FATAL and battery_voltage_fatal_flag == 0:
+        rospy.logfatal('battery voltage is low !')
+        battery_voltage_fatal_flag = 1
 
 
-def callback_get_command(twist.command):
+def callback_get_command_from_main(twist_command):
+    linear_vel = twist_command.linear.x
+    angular_vel = twist_command.angular.z
+
+
+def callback_get_command_from_joy(joy_msg):
+    teleop_lenear_vel = joy_msg.axes[1]
+    teleop_angular_vel = joy_msg.axes[3]
 
 
 if __name__ == '__main__':
@@ -37,7 +58,9 @@ if __name__ == '__main__':
                      callback_get_servo_info, queue_size=1)
 
     rospy.Subscriber('twist_command', Twist,
-                     callback_get_command, queue_size=1)
+                     callback_get_command_from_main, queue_size=1)
+
+    rospy.Subscriber('joy', Joy, callback_get_command_from_joy, queue_size=5)
 
     servo_command = Servo_command()
 
