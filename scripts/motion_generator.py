17f400d8ef3da846664938dd251d1d077c3c297c
k24koba <hozu.kazu.hozu@gmail.com>
2019-04-19 22:57:48 +0900

    update motion generator

--- scripts/motion_generator.py
+++ scripts/motion_generator.py
@@ -24,6 +24,9 @@ battery_voltage_fatal_flag = 0
 BATTERY_VOLTAGE_WARN = 14.2
 BATTERY_VOLTAGE_FATAL = 13.8
 
+teleop_lenear_vel = 0
+teleop_angular_vel = 0
+
 pitch = 0
 roll = 0
 
@@ -33,7 +36,6 @@ def callback_get_servo_info(info):
     encoder_count = info.encoder_count
     battery_voltage = 14.0
     velocity = info.motor_velocity
-    rospy.loginfo('hello')
 
     if battery_voltage < BATTERY_VOLTAGE_WARN and battery_voltage_warn_flag == 0:
         rospy.logwarn('battery voltage is low !')
@@ -53,42 +55,42 @@ def callback_get_posture(posture):
 def callback_get_command_from_main(twist_command):
     linear_vel = twist_command.linear.x
     angular_vel = twist_command.angular.z
+    # rospy.loginfo('hello')
 
 
 def callback_get_command_from_joy(joy_msg):
+    global teleop_lenear_vel, teleop_angular_vel
     teleop_lenear_vel = joy_msg.axes[1]
     teleop_angular_vel = joy_msg.axes[3]
 
 
 def generate_command():
-    pass
+    global teleop_lenear_vel, teleop_angular_vel
+    if teleop_lenear_vel != 0 or teleop_angular_vel != 0:
+        whipbot_motion.linear.x = teleop_lenear_vel
+        whipbot_motion.angular.z = teleop_angular_vel
+        pub_motor_control.publish(whipbot_motion)
 
 
 if __name__ == '__main__':
     rospy.init_node('motion_generator')
 
     pub_motor_control = rospy.Publisher(
-        'servo_command', Servo_command, queue_size=1)
+        'whipbot_motion', Twist, queue_size=1)
 
     rospy.Subscriber('servo_info', Servo_info,
                      callback_get_servo_info, queue_size=1)
-
     rospy.Subscriber('twist_command', Twist,
                      callback_get_command_from_main, queue_size=1)
-
     rospy.Subscriber('posture_angle', Posture_angle,
                      callback_get_posture, queue_size=1)
-
     rospy.Subscriber('joy', Joy, callback_get_command_from_joy, queue_size=5)
 
-    servo_command = Servo_command()
-
+    whipbot_motion = Twist()
     rate = rospy.Rate(50)
-
     while not rospy.is_shutdown():
         try:
             generate_command()
-            pub_motor_control.publish(servo_command)
 
         except IOError:
             pass
