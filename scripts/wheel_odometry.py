c9a281bfedcb6dd7be9a2d9c95f623bde1c45c5e
k24koba_laptop <hozu.kazu.hozu@gmail.com>
2019-05-01 08:34:41 +0900

    update wheel odometry node

--- scripts/wheel_odometry.py
+++ scripts/wheel_odometry.py
@@ -24,8 +24,9 @@ WHEEL_DIAMETER = 0.1524 # wheel diameter of the robot[m] : 6 inch
 TREAD = 0.26 # tread width of the robot[m] 294mm - 34.925mm
 
 # variables for wheel odometry of the robot
-encoder_count = []
-motor_velocity = []
+current_encoder_count = [0, 0]
+last_encoder_count = [0, 0]
+motor_velocity = [0, 0]
 velocity_left = 0 # velocity to ground [m/s]
 velocity_right = 0 # velocity to ground [m/s]
 odometry_count = 0
@@ -40,23 +41,8 @@ BATTERY_VOLTAGE_WARN = 14200 #publish a warning when battery voltage is lower th
 BATTERY_VOLTAGE_FATAL = 13900 #publish warnings when battery voltage is lower than 13900 [mV] (13.9V or 3.475V/cell)
 
 
-def callback_get_servo_info(servo_info):
-    global battery_voltage_warn_flag, battery_voltage_fatal_flag, encoder_count, motor_velocity
-    encoder_count = servo_info.encoder_count
-    battery_voltage = servo_info.input_voltage
-    motor_velocity = servo_info.motor_velocity
-
-    if battery_voltage[0] < BATTERY_VOLTAGE_WARN and battery_voltage_warn_flag == 0:
-        rospy.logwarn('battery voltage is low !')
-        battery_voltage_warn_flag_left = 1
-    elif battery_voltage[0] < BATTERY_VOLTAGE_FATAL:
-        rospy.logfatal('battery voltage is fatally low !')
-
-    calculate_odometry()
-
-
 def main_function():
-    global encoder_count, motor_velocity, velocity_left, velocity_right, odometry_count
+    global odometry_count, current_time, last_time, robot_location, robot_orientation, robot_velocity
     rospy.init_node('wheel_odometry')
 
     wheel_odometry_pub = rospy.Publisher(
@@ -93,6 +79,33 @@ def main_function():
             pass
 
         rate.sleep()
+
+
+
+def callback_get_servo_info(servo_info):
+    global battery_voltage_warn_flag, battery_voltage_fatal_flag, current_encoder_count, motor_velocity
+    current_encoder_count = servo_info.encoder_count
+    battery_voltage = servo_info.input_voltage
+    motor_velocity = servo_info.motor_velocity
+
+    if battery_voltage[0] < BATTERY_VOLTAGE_WARN and battery_voltage_warn_flag == 0:
+        rospy.logwarn('battery voltage is low !')
+        battery_voltage_warn_flag_left = 1
+    elif battery_voltage[0] < BATTERY_VOLTAGE_FATAL:
+        rospy.logfatal('battery voltage is fatally low !')
+
+    calculate_odometry()
+
+
+def calculate_odometry):
+    global robot_location, robot_orientation, robot_velocity, velocity_left, velocity_right, current_encoder_count, last_encoder_count, motor_velocity, current_time, last_time, dt
+
+    delta_encoder = [x-y for (x,y) in zip(current_encoder_count, last_encoder_count)]
+    velocity_left = ((delta_encoder[0] / PULSE_PER_ROUND) * pi * WHEEL_DIAMETER) / dt
+    velocity_right = ((delta_encoder[1] / PULSE_PER_ROUND) * pi * WHEEL_DIAMETER) / dt
+
+    robot_velocity[0] = (velocity_left + velocity_right) / 2.0
+    robot_velocity[1] = (velocity_right - velocity_left) / (TREAD / 2.0)
 
 
 
