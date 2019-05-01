7818e7807aea0b880e3cef5ec3549ee44902e895
k24koba_laptop <hozu.kazu.hozu@gmail.com>
2019-05-01 19:22:40 +0900

    update motion generator

--- scripts/motion_generator.py
+++ scripts/motion_generator.py
@@ -34,6 +34,8 @@ heading = 0
 # variables for position and velocity of the robot
 current_robot_location = [0.0, 0.0, 0.0] #current relative location from a start point : [x, y, theta]
 last_robot_location = [0.0, 0.0, 0.0] #last relative location from a start point : [x, y, theta]
+delta_robot_location = [0.0, 0.0, 0.0] #difference fo relative location between current frame and last frame : [x, y, theta]
+accumulated_error_of_robot_location = [0.0, 0.0, 0.0] #accumulated error of relative location from a start point of hovering : [x, y, theta]
 robot_velocity = [0.0, 0.0] #robot velocity : [linear vel, angular vel]
 
 def callback_get_posture(posture):
@@ -63,18 +65,29 @@ def callback_get_odometry(wheel_odometry):
 
 
 def generate_command():
-    global joy_lenear_vel, joy_angular_vel, main_linear_vel, main_angular_vel
-    
+    global joy_lenear_vel, joy_angular_vel, main_linear_vel, main_angular_vel, current_robot_location, last_robot_location, accumulated_error_of_robot_location
+
     if joy_lenear_vel != 0 or joy_angular_vel != 0:
+        accumulated_error_of_robot_location = [0.0, 0.0, 0.0] #reset accumulated location error during robot's hovering
         whipbot_motion.linear.x = joy_lenear_vel
         whipbot_motion.angular.z = joy_angular_vel
         pub_motion_control.publish(whipbot_motion)
 
     elif main_linear_vel != 0 or main_angular_vel != 0:
+        accumulated_error_of_robot_location = [0.0, 0.0, 0.0] #reset accumulated location error during robot's hovering
         whipbot_motion.linear.x = main_linear_vel
         whipbot_motion.angular.z = main_angular_vel
         pub_motion_control.publish(whipbot_motion)
 
+    else:
+        for i in range(3):
+            delta_robot_location[i] = current_robot_location[i] - last_robot_location[i]
+            accumulated_error_of_robot_location[i] = accumulated_error_of_robot_location[i] + delta_robot_location[i]
+
+        whipbot_motion.linear.x = hovering_linear_vel
+        whipbot_motion.angular.z = hovering_angular_vel
+        pub_motion_control.publish(whipbot_motion)
+        last_robot_location = current_robot_location
 
 if __name__ == '__main__':
     rospy.init_node('motion_generator')
